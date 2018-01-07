f1921b644054dd0413a466d317ab63e084956f6f
niggo1243 <nikog3@tlen.pl>
2018-01-07 17:46:36 +0100

    webservice glassfish test working!!!

--- src/main/quizServer/MainClass.java
+++ src/main/quizServer/MainClass.java
@@ -1,14 +1,18 @@
 package main.quizServer;
 
-import com.sun.net.httpserver.HttpsServer;
+import main.quizServer.rest.QuizService;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.net.URI;
 
 
-public final class MainClass
+public class MainClass
 {
-    public static final String serverRootUri = "https://localhost:8080/";
+    public static final String serverRootUri = "https://localhost:8080";
     public static final String defaultIpAdress = "127.0.0.1";
 
     public static final int defaultPort = 8080;
@@ -20,20 +24,25 @@ public final class MainClass
 
         try
         {
+            ResourceConfig rc = new ResourceConfig();
+            rc.register(QuizService.class);
 
-
-            //instantiate the socketAdress
-            InetSocketAddress inetSocketAddress = new InetSocketAddress(defaultIpAdress, defaultPort);
-            //create and start the main.quizServer
-            HttpsServer quizServer = HttpsServer.create(inetSocketAddress, defaultBackLog);
-
+            HttpServer quizServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(serverRootUri), rc);
+//
+//            //instantiate the socketAdress
+//            InetSocketAddress inetSocketAddress = new InetSocketAddress(defaultIpAdress, defaultPort);
+//            //create and start the main.quizServer
+//            //HttpsServer quizServer = HttpsServer.create(inetSocketAddress, defaultBackLog);
+//            HttpServer quizServer = HttpServer.create(inetSocketAddress, defaultBackLog);
+//
+//
             quizServer.start();
 
             System.out.println("Hit any key, to close the server: ");
             System.in.read();
 
             //stop the server
-            quizServer.stop(1);
+            quizServer.stop();
         }
         catch (IOException ioe)
         {
